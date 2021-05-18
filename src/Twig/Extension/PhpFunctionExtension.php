c566042298cacf3e4fb1b37532a9c64c0eada234
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2021-05-18 16:40:43 +0200

    added native PhpFunctionExtension

---
+++ src/Twig/Extension/PhpFunctionExtension.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\TwigBundle\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class PhpFunctionExtension extends AbstractExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('str_pad', 'str_pad'),
+            new TwigFilter('url_decode', 'url_decode'),
+            new TwigFilter('basename', 'basename'),
+            new TwigFilter('pathinfo', 'pathinfo'),
+        ];
+    }
+}
