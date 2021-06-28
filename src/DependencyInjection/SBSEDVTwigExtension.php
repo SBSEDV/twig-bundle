5f5d7800243d718f0bfe5e96ae4679ae6ccf3071
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2021-06-28 20:28:35 +0200

    added JsRoutingExtension

--- src/DependencyInjection/SBSEDVTwigExtension.php
+++ src/DependencyInjection/SBSEDVTwigExtension.php
@@ -4,14 +4,21 @@ namespace SBSEDV\Bundle\TwigBundle\DependencyInjection;
 
 use SBSEDV\Bundle\TwigBundle\EventListener\TimezoneListener;
 use SBSEDV\Bundle\TwigBundle\Twig\Extension\CookieConfigExtension;
+use SBSEDV\Bundle\TwigBundle\Twig\Extension\JsRoutingExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class SBSEDVTwigExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader(
@@ -35,6 +42,17 @@ class SBSEDVTwigExtension extends Extension
                 ->replaceArgument('$cookieName', $config['event_listeners']['TimezoneListener']['cookie_name'])
                 ->replaceArgument('$headerName', $config['event_listeners']['TimezoneListener']['header_name'])
                 ->replaceArgument('$sessionName', $config['event_listeners']['TimezoneListener']['session_name']);
+        }
+
+        if (array_key_exists('FOSJsRoutingBundle', $container->getParameter('kernel.bundles'))) {
+            $container->setDefinition(JsRoutingExtension::class, new Definition(JsRoutingExtension::class));
+
+            $container->getDefinition(JsRoutingExtension::class)
+                ->setArgument('$exposedRoutesExtractor', new Reference('fos_js_routing.extractor'))
+                ->setArgument('$serializer', new Reference('fos_js_routing.serializer'))
+                ->setArgument('$requestStack', new Reference(RequestStack::class))
+                ->setArgument('$debug', $container->getParameter('kernel.debug'))
+                ->addTag('twig.extension');
         }
     }
 }
